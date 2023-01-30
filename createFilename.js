9817025e5e26bfa3901a3e0f022284d4b0d57860
peishuangheng <peishuangheng@hualala.com>
2023-01-30 17:13:13 +0800

    feat: uypdate fileNames

--- createFilename.js
+++ createFilename.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const pathsToCheck = ['./']
-fs.readdir('./',(err,files)=>{
-    console.log(files)
+async function init(){
+    debugger
+    const data = await fs.readFileSync(path.resolve('files.json'), 'utf8')
+    const oldFiles = JSON.parse(data) || []
+    let files = (fs.readdirSync(path.resolve('./')) || []).concat(oldFiles)
+    files = Array.from(new Set(files)).filter(item=>!item.startsWith('.'))
     fs.writeFile(path.join(__dirname, 'files.json'), JSON.stringify(files.filter((item)=>item[0]!=='.')), 'utf-8',()=>{})
-})
+}
+init()
